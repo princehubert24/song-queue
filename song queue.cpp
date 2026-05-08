a1849166de7d76295060fd40559a2eaa05603e62
princehubert24 <princehubert.cagampang.24@usjr.edu.ph>
2026-05-08 22:14:01 +0800

    Add files via upload

---
+++ song queue.cpp
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Song {
+    char title[100];
+    char artist[100];
+    int duration;
+    struct Song* next;
+};
+
+struct Song* head = NULL;
+struct Song* tail = NULL;
+int size = 0;
+
+struct Song* createNode(char title[], char artist[], int duration) {
+    struct Song* newSong = (struct Song*)malloc(sizeof(struct Song));
+
+    strcpy(newSong->title, title);
+    strcpy(newSong->artist, artist);
+    newSong->duration = duration;
+    newSong->next = NULL;
+
+    return newSong;
+}
+
+int isEmpty() {
+    return head == NULL;
+}
+
+void addSong(char title[], char artist[], int duration) {
+    struct Song* newSong = createNode(title, artist, duration);
+
+    if (isEmpty()) {
+        head = newSong;
+        tail = newSong;
+    } else {
+        tail->next = newSong;
+        tail = newSong;
+    }
+
+    size++;
+    printf("-> Added: %s by %s\n", title, artist);
+}
+
+void playNext() {
+    if (isEmpty()) {
+        printf("ERROR: The playlist is currently empty! Add some songs first.\n");
+        return;
+    }
+
+    struct Song* temp = head;
+
+    printf("... Now Playing ...\n");
+    printf("-> Dequeued and Played: %s (%s)\n", temp->title, temp->artist);
+
+    head = head->next;
+    size--;
+
+    if (head == NULL) {
+        tail = NULL;
+    }
+
+    free(temp);
+}
+
+void viewPlaylist() {
+    if (isEmpty()) {
+        printf("Playlist is empty.\n");
+        return;
+    }
+
+    struct Song* current = head;
+    int i = 1;
+
+    printf("\n--- Current Playlist (Next Up is Song 1) ---\n");
+
+    while (current != NULL) {
+        printf("[%d] %s (%s) - %d sec\n",
+               i,
+               current->title,
+               current->artist,
+               current->duration);
+
+        current = current->next;
+        i++;
+    }
+
+    printf("-------------\n");
+}
+
+void getTotalDuration() {
+    struct Song* current = head;
+    int total = 0;
+
+    if (isEmpty()) {
+        printf("Total Duration: 0 seconds\n");
+        return;
+    }
+
+    while (current != NULL) {
+        total += current->duration;
+        current = current->next;
+    }
+
+    printf("Total Duration of Playlist: %d seconds (%d min, %d sec)\n",
+           total, total / 60, total % 60);
+}
+
+int main() {
+    int choice, num;
+
+    char title[20][100] = {
+        "Stairway to Heaven",
+        "Bohemian Rhapsody",
+        "Imagine",
+        "Hey Jude",
+        "Hotel California",
+        "Shape of You",
+        "Blinding Lights",
+        "Someone Like You",
+        "Smells Like Teen Spirit",
+        "Perfect",
+        "Believer",
+        "Thunder",
+        "Counting Stars",
+        "Faded",
+        "Let Her Go",
+        "Demons",
+        "Radioactive",
+        "Senorita",
+        "Peaches",
+        "Save Your Tears"
+    };
+
+    char artist[20][100] = {
+        "Led Zeppelin",
+        "Queen",
+        "John Lennon",
+        "The Beatles",
+        "The Eagles",
+        "Ed Sheeran",
+        "The Weeknd",
+        "Adele",
+        "Nirvana",
+        "Ed Sheeran",
+        "Imagine Dragons",
+        "Imagine Dragons",
+        "OneRepublic",
+        "Alan Walker",
+        "Passenger",
+        "Imagine Dragons",
+        "Imagine Dragons",
+        "Shawn Mendes & Camila Cabello",
+        "Justin Bieber",
+        "The Weeknd"
+    };
+
+    int duration[20] = {
+        482, 355, 181, 431, 390,
+        240, 200, 285, 301, 263,
+        204, 187, 257, 212, 244,
+        177, 186, 191, 198, 215
+    };
+
+    printf("*** MUSIC PLAYLIST (LINKED LIST QUEUE) ***\n");
+
+    do {
+        printf("\n--- Initial Song Library ---\n");
+        for (int i = 0; i < 20; i++) {
+            printf("[%d] %s (%s) - %d sec\n",
+                   i + 1, title[i], artist[i], duration[i]);
+        }
+
+        printf("-----------------------------\n");
+
+        printf("\n** Playlist Menu **\n");
+        printf("1. Add Song to Playlist (Enqueue)\n");
+        printf("2. View Current Playlist\n");
+        printf("3. Play Next Song (Dequeue)\n");
+        printf("4. Show Total Duration\n");
+        printf("5. Exit\n");
+        printf("-----------------------------\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("Enter song number to add (1-20): ");
+            scanf("%d", &num);
+
+            if (num >= 1 && num <= 20) {
+                addSong(title[num - 1], artist[num - 1], duration[num - 1]);
+            } else {
+                printf("Invalid choice.\n");
+            }
+        }
+        else if (choice == 2) {
+            viewPlaylist();
+        }
+        else if (choice == 3) {
+            playNext();
+        }
+        else if (choice == 4) {
+            getTotalDuration();
+        }
+
+    } while (choice != 5);
+
+    printf("Exiting Playlist Program. Goodbye!\n");
+
+    return 0;
+}
